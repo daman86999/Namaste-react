7088d6cae32ec11d366fcfeb49e7aa0a48a6db73
damanpreet <daman.086999@gmail.com>
2022-12-25 23:28:03 +0530

    coding assingment added

---
+++ Assignment-1/coding/app.js
@@ -0,0 +1,3 @@
+const helloReact = React.createElement("h1",{},"Hello World in React");
+const rootForReact = ReactDOM.createRoot(document.getElementById("rootForReact"));
+rootForReact.render(helloReact);
